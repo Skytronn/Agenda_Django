7fe770359c74a0f3fbfe0162e1b8532f2a3f1110
unknown <leonardogarciasou@gmail.com>
2020-09-26 23:06:01 -0300

    Adicionado tela de login e Logout

--- agenda_django/urls.py
+++ agenda_django/urls.py
@@ -7,8 +7,14 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agenda', views.lista_eventos),
+    path('agenda/', views.lista_eventos),
 
     #com o RedirectView.as_view pode se direcionar uma url tambem, assim como a função index
-    path('', RedirectView.as_view(url='/agenda/'))
+    path('', RedirectView.as_view(url='/agenda/')),
+
+#este url chama o def: login_user que irá direcionar para a pagina login.html
+    path('login/', views.login_user),
+
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
